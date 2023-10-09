3b43cd4c631b28ed9fc5df74fbf496676e83a137
Frank <wanyoike39@gmail.com>
2023-10-09 20:17:17 +0300

    first two tasks - markdown to html

---
+++ markdown2html.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""markdown to html script"""
+import sys
+import os
+import markdown
+
+
+def markdown2html(markdown_file, html_file):
+    with open(markdown_file, 'r') as file:
+        markdown_text = file.read()
+
+    html_output = markdown.markdown(markdown_text)
+
+    with open(html_file, 'w') as f:
+        f.write(html_output)
+        f.write("\n")
+    
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
+
+    markdown_file = sys.argv[1]
+    html_file = sys.argv[2]
+
+    if not os.path.exists(markdown_file):
+        sys.stderr.write("Missing {}\n".format(markdown_file))
+        exit(1)
+
+    markdown2html(markdown_file, html_file)
